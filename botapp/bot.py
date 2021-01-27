9f7560982d8f5e7b4741e80484c090ee3055c0d3
svinerus <svinerus@gmail.com>
2021-01-27 22:54:25 +0200

    test ready

--- botapp/bot.py
+++ botapp/bot.py
@@ -1,9 +1,11 @@
+from os import getenv
+
 from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from botapp.utils import DeepLinkFilter
 
-TOKEN = ""
+TOKEN = getenv('bot_token')
 
 storage = MemoryStorage()
 bot = Bot(TOKEN, parse_mode='HTML')
--- garni_studenti/urls.py
+++ garni_studenti/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 import parserapp.views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('test/', parserapp.views.parse)
+    path('/', admin.site.urls),
+    path('parse/', parserapp.views.parse)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
