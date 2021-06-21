f5c5bb592a6015431078a7b5ea6bb55cf690bf40
svinerus <svinerus@gmail.com>
2021-06-21 18:17:34 +0300

    set capture output

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [[ -v BOT_TOKEN ]]; then
-    gunicorn --workers 1 --bind unix:/run/telegram_bot.sock botapp:gunicorn --worker-class aiohttp.GunicornWebWorker
+    gunicorn --workers 1 --bind unix:/run/telegram_bot.sock botapp:gunicorn --worker-class aiohttp.GunicornWebWorker --capture-output
 elif [[ -v ADMIN ]]; then
-    gunicorn --workers 4 --bind unix:/run/django_admin.sock garni_studenti.wsgi:application
+    gunicorn --workers 4 --bind unix:/run/django_admin.sock garni_studenti.wsgi:application --capture-output
 else
     echo "You must set BOT_TOKEN to run bot or ADMIN to run django admin"
 fi
