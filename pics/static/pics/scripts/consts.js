ec9f649b31cc53390519cb166bdf2f31daa9b1f3
svinerus <svinerus@gmail.com>
2021-11-11 00:35:56 +0200

    fix radial

--- pics/static/pics/scripts/consts.js
+++ pics/static/pics/scripts/consts.js
@@ -40,7 +40,7 @@ export const captions = {
 
 export const radialQuestions = {  // questions order important!
     eng: ['meaningfulness', 'cheating', 'comfort', 'find_out_rating', 'politeness', 'punctuality', 'questions_available', 'relevance', 'sufficiency', 'grading_system'],
-    lector: ['grading_system', 'meaningfulness', 'comfort', 'conformity', 'find_out_rating', 'politeness', 'punctuality', 'questions_available', 'relevance', 'sufficiency', 'grading_system'],
+    lector: ['grading_system', 'meaningfulness', 'comfort', 'conformity', 'find_out_rating', 'politeness', 'punctuality', 'questions_available', 'relevance', 'sufficiency'],
     practic: ['comfort', 'find_out_rating', 'politeness', 'punctuality', 'relevance', 'grading_system', 'skills'],
     lector_practic: ['grading_system', 'meaningfulness', 'skills', 'cheating_l', 'comfort', 'conformity', 'find_out_rating', 'politeness', 'punctuality', 'questions_available', 'relevance', 'sufficiency', 'cheating_p'],
 }
